042d5cddc0826728a1cc80a2e9262ce43ed41fa6
facu ierullo <ierullofacu@gmail.com>
2025-02-27 13:25:48 -0300

    27/02 guardado

--- js.js
+++ js.js
@@ -1,2 +1,34 @@
+const slider = document.querySelector('.slider');
+    let index = 0;
+    const totalImages = slider.children.length;
+    const imagesToShow = 3;
 
+    function nextSlide() {
+      index++;
+      if (index >= totalImages - imagesToShow + 1) {
+        index = 0; 
+      }
+      updateSlider();
+    }
 
+    function prevSlide() {
+      index--;
+      if (index < 0) {
+        index = totalImages - imagesToShow; 
+      }
+      updateSlider();
+    }
+
+    function updateSlider() {
+      const width = slider.children[0].clientWidth; 
+      slider.style.transform = `translateX(-${index * width}px)`;
+    }
+
+    window.addEventListener('scroll', function() {
+      const navbar = document.getElementById('navbar');
+      if (window.scrollY > 350) {
+        navbar.classList.add('fixed-top');
+      } else {
+        navbar.classList.remove('fixed-top');
+      }
+    });
